ecc54db1dfe5fbb2e5f932c28db032ea91246cd4
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-02-06 19:31:39 +0200

    Update build_and_push.sh

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Build and push the Node Dockerfile for x64
-docker buildx create --use
-docker buildx build --platform linux/amd64 -t andreivcodes/smscan-node:amd64 -f Dockerfile.node . --push
-
-# Build and push the Node Dockerfile for ARM64 (Apple Silicon)
-docker buildx create --use
-docker buildx build --platform linux/arm64 -t andreivcodes/smscan-node:arm64 -f Dockerfile.node . --push
-
 # Build and push the Smscan App Dockerfile for x64
 docker buildx create --use
 docker buildx build --platform linux/amd64 -t andreivcodes/smscan-app:amd64 -f Dockerfile.smscan --build-arg BUILDARCH=x86_64 . --push
@@ -15,3 +7,11 @@ docker buildx build --platform linux/amd64 -t andreivcodes/smscan-app:amd64 -f D
 # Build and push the Smscan App Dockerfile for ARM64 (Apple Silicon)
 docker buildx create --use
 docker buildx build --platform linux/arm64 -t andreivcodes/smscan-app:arm64 -f Dockerfile.smscan --build-arg BUILDARCH=arm64 . --push
+
+# Build and push the Node Dockerfile for x64
+docker buildx create --use
+docker buildx build --platform linux/amd64 -t andreivcodes/smscan-node:amd64 -f Dockerfile.node . --push
+
+# Build and push the Node Dockerfile for ARM64 (Apple Silicon)
+docker buildx create --use
+docker buildx build --platform linux/arm64 -t andreivcodes/smscan-node:arm64 -f Dockerfile.node . --push
